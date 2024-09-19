f06f985fb15c84145e8e3f1732245822b2b857fb
Jagermind <jagerunz@gmail.com>
2024-09-19 16:23:52 -0400

    added input statements and converted to Int data types

--- COP 1000 Assignment 3-2/Assignment 3-2.py
+++ COP 1000 Assignment 3-2/Assignment 3-2.py
@@ -3,7 +3,8 @@
 #Output: Accept or Reject
 
 # Get input and convert to correct data type for testScore and classRank
-
+testScore=int(input("Please input Test Score:"))
+classRank=int(input("Please input Class Rank:"))
 # Test using admission requirements and print Accept or Reject
 if testScore >= 90:
   if classRank >= 25:
