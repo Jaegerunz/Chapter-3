dd4850c21ad970cb8a41b7530f732db3143f3a4c
Jagermind <jagerunz@gmail.com>
2024-11-19 11:13:34 -0500

    Changed date checker ot include days with 30 or 31 days/ FEB

--- COP 1000 Assignment 3-3/Assignment 3-3.py
+++ COP 1000 Assignment 3-3/Assignment 3-3.py
@@ -21,17 +21,27 @@ day=input("Please input day:")
 
 # Check to be sure date is valid
 
-if int(year) <= MIN_YEAR: # invalid year
-    validDate = False
-elif int(month) < MIN_MONTH or int(month) > MAX_MONTH: # invalid month
-    validDate = False
-elif int(day) < MIN_DAY or int(day) > MAX_DAY: # invalid day
+if int(year) < MIN_YEAR: # invalid year
     validDate = False
 
+if int(month) < MIN_MONTH or int(month) > MAX_MONTH: # invalid month
+    validDate = False
+#31Days
+if month ==1 or month == 3 or month == 5 or month == 7 or month ==9 or month == 10 or month == 12:
+    if day <= MIN_DAY or day >= MAX_DAY:
+        validDate= False
+#30Days
+elif month == 4 or month == 6 or month ==9 or month ==11:
+    if day < MIN_DAY or day > 30:
+        validDate= False
+#Feb
+elif month ==2: 
+    if day < MIN_DAY or day >29:
+        validDate= False
 # Test to see if date is valid and output date and whether it is valid or not
 
 # endOfJob()
 if validDate == True:
-    print(month+"/"+ day +"/"+ year+" is a valid date" )
+    print(month+"/"+ day +"/"+ year+ " is a valid date" )
 else:
     print(month+"/"+ day +"/"+ year+ " is an invalid date" )
